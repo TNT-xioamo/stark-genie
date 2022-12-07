79744c6f77aef174d26700053716723a18a02883
xingliu <liuxing@easthome>
2022-12-07 14:33:44 +0800

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mac_app/main.dart';
+import 'package:stark_genie/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
