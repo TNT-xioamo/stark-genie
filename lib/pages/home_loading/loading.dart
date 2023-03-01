33849b2bc4b0286ca97ba071f5313cc0961ea121
xingliu <liuxing@easthome>
2023-03-01 16:40:05 +0800

    handleToken

--- lib/pages/home_loading/loading.dart
+++ lib/pages/home_loading/loading.dart
@@ -11,26 +11,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  handleToken() async{
+  handleToken() async {
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('user_token') ?? '';
     debugPrint('user_token: $token');
-    if(token == ''){
-      Navigator.of(context).pushNamedAndRemoveUntil('/StarkLogin', (route) => route == null,);
+    if (token == '') {
+      Navigator.of(context).pushNamedAndRemoveUntil(
+        '/StarkLogin',
+        (route) => route == null,
+      );
     }
   }
+
   @override
   void initState() {
     super.initState();
     Future.delayed(Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacementNamed("/StarkLogin");
+      handleToken();
+      // Navigator.of(context).pushReplacementNamed("/StarkLogin");
     });
   }
 
   @override
   Widget build(BuildContext context) {
     //启动图片
-    return  Material(
+    return Material(
       child: Image.asset(
         "images/4x/home_qd.jpg",
         fit: BoxFit.fill,
