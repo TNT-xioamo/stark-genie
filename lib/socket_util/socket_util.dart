18a818fcadbfb03c2a4fc461fec2fa4e29465538
xingliu <liuxingcoco@iCloud.com>
2023-03-08 09:53:07 +0800

    ws：ip

--- lib/socket_util/socket_util.dart
+++ lib/socket_util/socket_util.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-const String _SOCKET_URL = 'ws://172.16.0.19:8100/notice';
+const String _SOCKET_URL = 'ws://172.16.0.19:9001/message/notice';
 
 // Map<String, dynamic> headers = new Map();
 
